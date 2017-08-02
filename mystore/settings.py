a691cacf4d0425350538da223797a3b86dcacea5
Adley Lu <lu.adley@gmail.com>
2017-08-02 20:40:05 +0800

    Add auth

--- mystore/settings.py
+++ mystore/settings.py
@@ -114,7 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = '/'
 
+LOGIN_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
