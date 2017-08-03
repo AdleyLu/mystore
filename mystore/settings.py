fcbb81b4b9e685f63b9976f23b0921c9635d212f
Adley Lu <lu.adley@gmail.com>
2017-08-03 19:31:21 +0800

    HeroKu

--- mystore/settings.py
+++ mystore/settings.py
@@ -115,12 +115,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+
 LOGIN_REDIRECT_URL = '/'
 
 LOGIN_URL = '/login/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
